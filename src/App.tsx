8e79e6c54721f98e045f150c1421b21a7bb0bb64
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-10-13 18:04:13 +0530

    :sparkles: meh

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialStore = [{
+  id: 1,
+  name: 'Chicken Momo',
+  image: 'http://spicyworld.in/recipeimages/chicken-momo.jpg',
+  price: 10.50,
+  count: 4
+}, {
+  id: 2,
+  name: 'Spicy Mexican Potatoes',
+  image: 'https://cdn.images.express.co.uk/img/dynamic/14/285x214/272187_1.jpg',
+  price: 8.50,
+  count: 0
+}, {
+  id: 3,
+  name: 'PBJ Sandwich',
+  image: 'https://www.landolakes.com/RecipeManagementSystem/media/Recipe-Media-Files/Recipes/Retail/DesktopImages/2018_Grilled-Peanut-Butter-and-Jelly_20336_600x600.jpg?ext=.jpg',
+  price: 5.90,
+  count: 0
+}]
+
+class App extends React.Component {
+
+  state = {
+    store: initialStore
+  }
+
+  decrementItem = (item: any) => {
+    let { store } = this.state
+    store[store.indexOf(item)].count > 0 ?
+    store[store.indexOf(item)].count -= 1 :
+    store[store.indexOf(item)].count = 0
+    this.setState({store})
+  }
+
+  incrementItem = (item: any) => {
+    let { store } = this.state
+    store[store.indexOf(item)].count += 1
+    this.setState({store})
+  }
+
+  render () {
+    const { store } = this.state
+    // TODO: add a cart checkout functionality
+    return (
+      <div className="container App">
+        <div className="row">
+          {store && store.map((item: any) => (
+            <div key={item.id} className="thumbnail">
+              <img src={item.image} alt={item.name}/>
+              <p>{item.name}</p>
+              <p>${item.price}</p> 
+              <div className="btn-grp">
+                <button onClick={() => this.decrementItem(item)}>
+                  -
+                </button>
+                <button onClick={() => this.incrementItem(item)}>
+                  +
+                </button>
+                <span>
+                  {item.count}
+                </span>
+              </div> 
+              <br/>
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
